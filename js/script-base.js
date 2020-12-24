c3e545b82ff9b02b47b162cdfec4016026ff8c07
unknown <18043@stemegypt.edu.eg>
2020-12-24 22:39:51 +0200

    local

--- js/script-base.js
+++ js/script-base.js
@@ -30,37 +30,56 @@ $(document).ready(function() {
                     // Extract data
                     var src = '<img src="' + $(el).children()[0].dataset.big + '" alt="gallery item">';
                     var update = $('.show-gallery-item-on-hover');
-                    update.html("");
-
-                    // Calculate new position
-                    pos = $(el).offset();
-                    pos.right = Math.round(pos.left + $(el).width());
-                    pos.bottom = Math.round($(el).height() + pos.top);
-                    pos.width = $(el).children().prop('naturalWidth');
-                    pos.height = $(el).children().prop('naturalHeight');
-                    pos.midY = (pos.top + pos.bottom) / 2;
-                    pos.midX = (pos.left + pos.right) / 2;
-                    pos.newY = pos.midY - pos.height / 2 >= 0 ?
-                        pos.midY - pos.height / 2 : 0;
-                    pos.newX = Math.round(pos.right - $(window).width()) >= -20 ?
-                        pos.right - pos.width : pos.midX - pos.width / 2 >= 0 ?
-                        pos.midX - pos.width / 2 : 0;
-
-                    // Update elements
-                    update.html(src);
                     update.css({
-                        top: pos.newY,
-                        left: pos.newX,
-                        zIndex: 10,
-                        boxShadow: "0px 0px 10px rgba(32, 32, 32, 0.6)",
                         opacity: 0
                     });
-                    update.animate({
-                        opacity: 1
-                    }, 600);
+                    update.html("");
+                    update.html(src);
                     setTimeout(() => {
-                        $('.show-gallery-item-on-hover').mouseleave();
-                    }, 30000);
+                        // Adjust preview dimensions (responsive)
+                        var nH = update.children().prop('naturalHeight');
+                        var nW = update.children().prop('naturalWidth');
+                        if (update.children().prop('naturalWidth') > 2 * $(el).width() && nW >= nH) {
+                            update.children().width(2 * $(el).width());
+                            update.children().height(nH * ((2 * $(el).width()) / nW));
+                        } else if (update.children().prop('naturalHeight') > 2 * $(el).width()) {
+                            update.children().height(2 * $(el).width());
+                            update.children().width(nW * ((2 * $(el).height()) / nH));
+                        }
+                        console.log(nH, nW);
+
+                        // Calculate new position
+                        pos = $(el).parent().offset();
+                        pos.right = Math.round(pos.left + $(el).width());
+                        pos.bottom = Math.round($(el).height() + pos.top);
+                        pos.width = update.children().width() !== 0 ? update.children().width() : nW;
+                        pos.height = update.children().height() !== 0 ? update.children().height() : nH;
+                        pos.midY = (pos.top + pos.bottom) / 2;
+                        pos.midX = (pos.left + pos.right) / 2;
+                        pos.newY = Math.round(pos.midY - (pos.height / 2) - $('.navbar').offset().top) <= 72.5 ?
+                            pos.top : pos.midY - pos.height / 2 >= 0 ?
+                            pos.midY - pos.height / 2 : 0;
+                        pos.newX = Math.round(pos.right - $(window).width()) >= -20 ?
+                            pos.right - pos.width : pos.midX - pos.width / 2 >= 0 ?
+                            pos.midX - pos.width / 2 : 0;
+                        console.log(pos);
+
+                        // Update elements
+                        update.parent()[0].dataset.content = $(el).children()[0].dataset.display;
+                        update.css({
+                            top: pos.newY,
+                            left: pos.newX,
+                            zIndex: 10,
+                            boxShadow: "0px 0px 10px rgba(32, 32, 32, 0.6)",
+                            opacity: 0
+                        });
+                        update.animate({
+                            opacity: 1
+                        }, 600);
+                        setTimeout(() => {
+                            $('.show-gallery-item-on-hover').mouseleave();
+                        }, 30000);
+                    }, 50);
                 }
 
             });
@@ -86,25 +105,32 @@ $(document).ready(function() {
         $(function() {
             $('.pop-up-call').each(function(i, el) {
                 $(el).click(function() {
-                    $('#pop-up').removeClass('hidden');
+                    $('#pop-up').removeClass('remove');
                     $('#pop-up-window').addClass('pop-up-window-animate');
-                    $('#pop-up-content').html('<div style="display: flex; flex: 100%; width: 100%; height: 100%; align-items: center; justify-content: center;"><img src="https://i.stack.imgur.com/oQ0tF.gif" width="40px" alt="load"></div>');
-                    $.ajax({
-                        type: 'GET',
-                        url: el.dataset.content,
-                        timeout: 5000,
-                        success: function(data) {
-                            $('#pop-up-content').html(data);
-                        },
-                        error: function(a, b) {
-                            setTimeout(() => {
-                                $('#pop-up-close').click();
-                            }, 1000)
-                        }
-                    });
+                    if (el.dataset.type === "XHTML") {
+                        $('#pop-up-content').html('<div style="display: flex; flex: 100%; width: 100%; height: 100%; align-items: center; justify-content: center;"><img src="https://i.stack.imgur.com/oQ0tF.gif" width="40px" alt="load"></div>');
+                        $('#pop-up-close').removeClass('remove');
+                        $.ajax({
+                            type: 'GET',
+                            url: el.dataset.content,
+                            timeout: 5000,
+                            success: function(data) {
+                                $('#pop-up-content').html(data);
+                            },
+                            error: function(a, b) {
+                                setTimeout(() => {
+                                    $('#pop-up-close').click();
+                                }, 1000)
+                            }
+                        });
+                    } else if (el.dataset.type === "image") {
+                        $('#pop-up-window').addClass('image-display');
+                        $('#image-pop-up-div').removeClass('remove');
+                        $('#image-display')[0].src = el.dataset.content;
+                    }
                 });
             });
-            $('#pop-up-close').click(function() {
+            $('.pop-up-close').click(function() {
                 $('#pop-up-window').removeClass('pop-up-window-animate');
                 setTimeout(() => {
                     $('#pop-up-window').addClass('pop-up-window-close-animate');
@@ -112,7 +138,10 @@ $(document).ready(function() {
                 setTimeout(() => {
                     $('#pop-up-content').html("");
                     $('#pop-up-window').removeClass('pop-up-window-close-animate');
-                    $('#pop-up').addClass('hidden');
+                    $('#pop-up-window').removeClass('image-display');
+                    $('#pop-up-close').addClass('remove');
+                    $('#image-pop-up-div').addClass('remove');
+                    $('#pop-up').addClass('remove');
                 }, 1200);
             });
         });
