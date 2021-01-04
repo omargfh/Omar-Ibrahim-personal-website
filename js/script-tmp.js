82f81d715b8b69f3450229794027d4a9aebfcb8a
unknown <18043@stemegypt.edu.eg>
2021-01-04 02:01:33 +0200

    commit

--- js/script-tmp.js
+++ js/script-tmp.js
@@ -1,4 +1,24 @@
-$(document).ready(function () {
+$(document).ready(function() {
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        margin: 50,
+        center: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
 
     $(window).on('load', function() {
         headerControl("onload");
