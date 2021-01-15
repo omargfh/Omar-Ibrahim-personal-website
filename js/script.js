38ab21cb1ab635e733c164d3610b2df177513bcd
unknown <18043@stemegypt.edu.eg>
2021-01-15 04:30:49 +0200

    updates

--- js/script.js
+++ js/script.js
@@ -23,11 +23,9 @@ $(document).ready(function() {
             })
         }
 
-        $(window).on('load', function() {
-            headerControl("onload");
-            $('.navbar-toggler').click(function() {
-                headerControl("onclick");
-            });
+        headerControl("onload");
+        $('.navbar-toggler').click(function() {
+            headerControl("onclick");
         });
 
         $(window).on('resize', function() {
@@ -38,41 +36,36 @@ $(document).ready(function() {
             headerControl("onscroll");
         });
     } else {
-        $(document).ready(function() {
-            $('.carousel-fast').carousel({
-                interval: 1000
-            });
-
-            $(window).on('load', function() {
-                headerControl("onload", "index");
-                $('.navbar-toggler').click(function() {
-                    headerControl("onclick", "index");
-                });
-            });
-
-            $(window).on('resize', function() {
-                headerControl("onload", "index");
-                changeText("#right-down", 767, "To the right, you can see a picture of me during the last STEM Model UN!", "If you look down, you can see a picture of me during the last STEM Model UN!");
-            });
-
-            $(window).on('scroll', function() {
-                headerControl("onscroll", "index");
-                showElements('#section-2', 'card');
-                showElements('#section-3-nav', 'timeline');
-                showElements('#section-1-nav', 'section-1');
-            });
+        $('.carousel-fast').carousel({
+            interval: 1000
         });
+
+        headerControl("onload", "index");
+        $('.navbar-toggler').click(function() {
+            headerControl("onclick", "index");
+        });
+
+        $(window).on('resize', function() {
+            headerControl("onload", "index");
+            changeText("#right-down", 767, "To the right, you can see a picture of me during the last STEM Model UN!", "If you look down, you can see a picture of me during the last STEM Model UN!");
+        });
+
+        $(window).on('scroll', function() {
+            headerControl("onscroll", "index");
+            showElements('#section-2', 'card');
+            showElements('#section-1-nav', 'section-1');
+            $('.event').each(function(i, el) {
+                showElements(el, 'timeline');
+            })
+        });
+        // Particle Js
+        particlesJS("particles-js", { "particles": { "number": { "value": 260, "density": { "enable": true, "value_area": 800 } }, "color": { "value": "#ffffff" }, "shape": { "type": "circle", "stroke": { "width": 0, "color": "#000000" }, "polygon": { "nb_sides": 5 }, "image": { "src": "img/github.svg", "width": 100, "height": 100 } }, "opacity": { "value": 1, "random": true, "anim": { "enable": true, "speed": 1, "opacity_min": 0, "sync": false } }, "size": { "value": 3, "random": true, "anim": { "enable": false, "speed": 4, "size_min": 0.3, "sync": false } }, "line_linked": { "enable": false, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1 }, "move": { "enable": true, "speed": 1, "direction": "none", "random": true, "straight": false, "out_mode": "out", "bounce": false, "attract": { "enable": false, "rotateX": 600, "rotateY": 600 } } }, "interactivity": { "detect_on": "canvas", "events": { "onhover": { "enable": true, "mode": "bubble" }, "onclick": { "enable": true, "mode": "repulse" }, "resize": true }, "modes": { "grab": { "distance": 400, "line_linked": { "opacity": 1 } }, "bubble": { "distance": 250, "size": 0, "duration": 2, "opacity": 0, "speed": 3 }, "repulse": { "distance": 400, "duration": 0.4 }, "push": { "particles_nb": 4 }, "remove": { "particles_nb": 2 } } }, "retina_detect": true });
     }
 
     // Activate Carousel
     $('.carousel').carousel({
         interval: 5000
     });
-
-    // Particle Js
-    particlesJS("particles-js", { "particles": { "number": { "value": 260, "density": { "enable": true, "value_area": 800 } }, "color": { "value": "#ffffff" }, "shape": { "type": "circle", "stroke": { "width": 0, "color": "#000000" }, "polygon": { "nb_sides": 5 }, "image": { "src": "img/github.svg", "width": 100, "height": 100 } }, "opacity": { "value": 1, "random": true, "anim": { "enable": true, "speed": 1, "opacity_min": 0, "sync": false } }, "size": { "value": 3, "random": true, "anim": { "enable": false, "speed": 4, "size_min": 0.3, "sync": false } }, "line_linked": { "enable": false, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1 }, "move": { "enable": true, "speed": 1, "direction": "none", "random": true, "straight": false, "out_mode": "out", "bounce": false, "attract": { "enable": false, "rotateX": 600, "rotateY": 600 } } }, "interactivity": { "detect_on": "canvas", "events": { "onhover": { "enable": true, "mode": "bubble" }, "onclick": { "enable": true, "mode": "repulse" }, "resize": true }, "modes": { "grab": { "distance": 400, "line_linked": { "opacity": 1 } }, "bubble": { "distance": 250, "size": 0, "duration": 2, "opacity": 0, "speed": 3 }, "repulse": { "distance": 400, "duration": 0.4 }, "push": { "particles_nb": 4 }, "remove": { "particles_nb": 2 } } }, "retina_detect": true });
-    // particlesJS("particles-js", { "particles": { "number": { "value": 100, "density": { "enable": false, "value_area": 800 } }, "color": { "value": "#ffffff" }, "shape": { "type": "star", "stroke": { "width": 0, "color": "#000000" }, "polygon": { "nb_sides": 5 }, "image": { "src": "http://wiki.lexisnexis.com/academic/images/f/fb/Itunes_podcast_icon_300.jpg", "width": 100, "height": 100 } }, "opacity": { "value": 0.5, "random": false, "anim": { "enable": false, "speed": 1, "opacity_min": 0.1, "sync": false } }, "size": { "value": 4, "random": true, "anim": { "enable": false, "speed": 40, "size_min": 0.1, "sync": false } }, "line_linked": { "enable": false, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1 }, "move": { "enable": true, "speed": 14, "direction": "left", "random": false, "straight": true, "out_mode": "out", "bounce": false, "attract": { "enable": false, "rotateX": 600, "rotateY": 1200 } } }, "interactivity": { "detect_on": "canvas", "events": { "onhover": { "enable": false, "mode": "grab" }, "onclick": { "enable": true, "mode": "repulse" }, "resize": true }, "modes": { "grab": { "distance": 200, "line_linked": { "opacity": 1 } }, "bubble": { "distance": 400, "size": 40, "duration": 2, "opacity": 8, "speed": 3 }, "repulse": { "distance": 200, "duration": 0.4 }, "push": { "particles_nb": 4 }, "remove": { "particles_nb": 2 } } }, "retina_detect": true });
-    // particlesJS("particles-js", { "particles": { "number": { "value": 241, "density": { "enable": true, "value_area": 1341.5509907748635 } }, "color": { "value": "#ffffff" }, "shape": { "type": "circle", "stroke": { "width": 0, "color": "#000000" }, "polygon": { "nb_sides": 10 }, "image": { "src": "img/github.svg", "width": 100, "height": 100 } }, "opacity": { "value": 1, "random": true, "anim": { "enable": true, "speed": 1, "opacity_min": 0, "sync": false } }, "size": { "value": 8.017060304327606, "random": true, "anim": { "enable": false, "speed": 4, "size_min": 0.3, "sync": false } }, "line_linked": { "enable": false, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1 }, "move": { "enable": true, "speed": 1, "direction": "none", "random": true, "straight": false, "out_mode": "out", "bounce": false, "attract": { "enable": false, "rotateX": 600, "rotateY": 600 } } }, "interactivity": { "detect_on": "canvas", "events": { "onhover": { "enable": true, "mode": "bubble" }, "onclick": { "enable": true, "mode": "repulse" }, "resize": true }, "modes": { "grab": { "distance": 400, "line_linked": { "opacity": 1 } }, "bubble": { "distance": 250, "size": 0, "duration": 2, "opacity": 0, "speed": 3 }, "repulse": { "distance": 400, "duration": 0.4 }, "push": { "particles_nb": 4 }, "remove": { "particles_nb": 2 } } }, "retina_detect": true });
 
     $(window).on('scroll', function() {
         $('.gallery-item').each(function(i, el) {
@@ -361,7 +354,8 @@ function isElementInViewport(el) {
 }
 
 function showElements(check, el) {
-    if (isElementInViewport(document.querySelector(check))) {
+    check = typeof check === 'string' ? document.querySelector(check) : check;
+    if (isElementInViewport(check)) {
         let elements = document.getElementsByClassName(el);
         for (let i = 0; i < elements.length; i++) {
             if (elements[i].classList.contains('visible-mobile')) {
